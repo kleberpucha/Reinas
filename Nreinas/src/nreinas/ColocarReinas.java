62944690541cc2742050b01b95c4db3f8c7c90d3
KLEBER PUCHA <KLEBER PUCHA@DESKTOP-E5F740O>
2018-10-22 00:00:44 -0500

    Version 0.4

--- Nreinas/src/nreinas/ColocarReinas.java
+++ Nreinas/src/nreinas/ColocarReinas.java
@@ -14,41 +14,50 @@ import java.io.IOException;
  * @author KLEBER PUCHA
  */
 public class ColocarReinas {
+        int cont=0;
+        int solucion=0;
         //Declaramos la variable para crear el archivo de log
         String dir="C://Users//KLEBER PUCHA//Desktop//";
         FileWriter archivo;
         //creamos un metodo public colocarReinas el mismo que nos permitira ir 
         //de alguna manera monitoreando la ubicacion de las reinas que se colocan
-        public void colocarReinas(int[][] a, int j) throws IOException {  this.dir = dir;
+        public void colocarReinas(int[][] a, int j) throws IOException {  
+        
+        this.dir = dir;
         if(new File(dir+"//"+"log.txt").exists()==false){
             archivo=new FileWriter(new File(dir+"//"+"log.txt"),false);
-            archivo.write("Recorrido ");
             archivo.close();
         }
         archivo=new FileWriter(new File(dir+"//"+"logReinas.txt"),true);
         //verificacmos si j que inicia con 0 es menor a la dimencion de la matriz
         //con esto logramos iniciar
         if(j<a.length){
+            
             for(int i = 0; i < a.length; i++){
                 //verificamos que si la ubicacion es valida
                 if(validarUbicacion(a,i,j)){
+                    cont++;
                     //si es true quiere decir q no reinas q se cruecen para la 
                     //la ubicacion actual y por ende colocamos una Reina
                     a[i][j]=1;
                     //escribimos la ubicacion de la reina en nuestro log
-                      archivo.write(i+","+j+"\r\n");
+                      archivo.write(cont+" - "+i+","+j+"\r\n");
                       archivo.close();
                     //continuamos con la nueva matriz colocada la Reina
                     //de forma de recursividad enviamos la nueva matriz y la j 
                     //le sumamos una ubicacion nueva
                     colocarReinas(a,j+1); 
                     a[i][j]=0;
+                    cont=0;
                 }
             }
         }
         else
         {
             //llamamos al metodo imprimir que contiene la matriz modificada
+            solucion++;
+            System.out.println("SOLUCION "+solucion);
+            System.out.println("FELIZ CUMPLE CARO");
             imprimirMatriz(a);
         }
     }
@@ -79,8 +88,7 @@ public class ColocarReinas {
                 for (int j = 0; j<a.length; j++) {
                     System.out.print(a1[j] + " ");
                 }
-                System.out.println();
+                System.out.println("");
             }
-        System.out.println();
     }
 }
