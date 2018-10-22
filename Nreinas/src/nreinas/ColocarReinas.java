a4e671ee49b0d142ba5ad3ee4f40c310d8ff96f5
KLEBER PUCHA <KLEBER PUCHA@DESKTOP-E5F740O>
2018-10-21 22:27:02 -0500

    Version 0.3

--- Nreinas/src/nreinas/ColocarReinas.java
+++ Nreinas/src/nreinas/ColocarReinas.java
@@ -12,24 +12,32 @@ package nreinas;
 public class ColocarReinas {
         //creamos un metodo public colocarReinas el mismo que nos permitira ir 
         //de alguna manera monitoreando la ubicacion de las reinas que se colocan
-        public void colocarReinas(char[][] a, int j) {
-        //verificacmos si j q inicia con 0 es menor 
+        public void colocarReinas(int[][] a, int j) {
+        //verificacmos si j que inicia con 0 es menor a la dimencion de la matriz
+        //con esto logramos iniciar
         if(j<a.length){
             for(int i = 0; i < a.length; i++){
+                //verificamos que si la ubicacion es valida
                 if(validarUbicacion(a,i,j)){
+                    //si es true quiere decir q no reinas q se cruecen para la 
+                    //la ubicacion actual y por ende colocamos una Reina
                     a[i][j]=1;
-                    colocarReinas(a,j+1);
+                    //continuamos con la nueva matriz colocada la Reina
+                    //de forma de recursividad enviamos la nueva matriz y la j 
+                    //le sumamos una ubicacion nueva
+                    colocarReinas(a,j+1); 
                     a[i][j]=0;
                 }
             }
         }
         else
         {
+            //llamamos al metodo imprimir que contiene la matriz modificada
             imprimirMatriz(a);
         }
     }
         
-    private static boolean validarUbicacion(char[][] a, int i, int j) {
+    private static boolean validarUbicacion(int[][] a, int i, int j) {
         boolean bandera = true;
         for(int k =0;k<a.length && bandera == true;k++){
             if(a[i][k]==1){
@@ -43,15 +51,15 @@ public class ColocarReinas {
                 }
                 if(m+n == i+j && a[m][n]==1){
                     bandera= false;
-                }
+               }
             }
         }
         return bandera;
     }
-        private static void imprimirMatriz(char[][] a) {
+        private static void imprimirMatriz(int[][] a) {
         for(int i =0;i < a.length;i++){
             for(int j=0;j<a.length;j++){ 
-                System.out.print(a[i][j]);
+                System.out.print(a[i][j]+" ");
             }
             System.out.println();
         }
--- Nreinas/src/nreinas/Nreinas.java
+++ Nreinas/src/nreinas/Nreinas.java
@@ -17,55 +17,16 @@ public class Nreinas {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
+      //declaramos un valor entero el que reperentara en numero de reinas
+      //A mas de eso servira como la dimencion del tablero
       int n=4;
+      //declaramos una variable a de tico matriz char ya que nos permitira
+      //colocar caracteres especiales para reprentar a la reina
       int a[][]= new int[n][n];
-      colocarReinas(a,0);
-    }
-
-    private static void colocarReinas(int[][] a, int j) {
-        if(j<a.length){
-            for(int i = 0; i < a.length; i++){
-                if(validarUbicacion(a,i,j)){
-                    a[i][j]=1;
-                    colocarReinas(a,j+1);
-                    a[i][j]=0;
-                }
-            }
-        }
-        else
-        {
-            imprimirMatriz(a);
-        }
-    }
-
-    private static boolean validarUbicacion(int[][] a, int i, int j) {
-        boolean bandera = true;
-        for(int k =0;k<a.length && bandera == true;k++){
-            if(a[i][k]==1){
-                bandera = false;
-            }
-        }
-        for(int m = 0 ; m < a.length && bandera == true; m++){
-            for(int n = 0;n <a.length && bandera == true; n++){
-                if(m-n == i-j && a[m][n]==1){
-                    bandera = false;
-                }
-                if(m+n == i+j && a[m][n]==1){
-                    bandera= false;
-                }
-            }
-        }
-        return bandera;
-    }
-    
-    private static void imprimirMatriz(int[][] a) {
-        for(int i =0;i < a.length;i++){
-            for(int j=0;j<a.length;j++){ 
-                System.out.print(a[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
+      //iniciamos el metodo colocarReinas que se encuentra en la clase ColocarReinas
+      //para esto enviamos la matriz a que contiene las dimenciones del tablero
+      //y un 0 que reperenta la ubicacion de inicio del tablero 
+      ColocarReinas cr = new ColocarReinas();
+      cr.colocarReinas(a, 0);
+    }   
 }
